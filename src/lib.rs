a053a7392354e1d0faf14de2813278259571ec91
Cecile Tonglet <cecile.tonglet@cecton.com>
2019-08-15 21:36:46 +0200

    Added: all the other gettext methods

--- src/lib.rs
+++ src/lib.rs
@@ -41,13 +41,26 @@ pub enum SerdeGetText {
         ngettext: SerdeGetTextNGetText,
         args: Option<Formatter>,
     },
-    /*
-    PGetText,
-    DGetText,
-    DNGetText,
-    NPGetText,
-    DCNGetText,
-    */
+    PGetText {
+        pgettext: SerdeGetTextPGetText,
+        args: Option<Formatter>,
+    },
+    DGetText {
+        dgettext: SerdeGetTextDGetText,
+        args: Option<Formatter>,
+    },
+    DNGetText {
+        dngettext: SerdeGetTextDNGetText,
+        args: Option<Formatter>,
+    },
+    NPGetText {
+        npgettext: SerdeGetTextNPGetText,
+        args: Option<Formatter>,
+    },
+    DCNGetText {
+        dcngettext: SerdeGetTextDCNGetText,
+        args: Option<Formatter>,
+    },
 }
 
 impl SerdeGetText {
@@ -86,6 +99,11 @@ impl SerdeGetText {
             SerdeGetText::Datetime(x) => (x.to_string(), &None),
             SerdeGetText::GetText { gettext, args } => (gettext.translate(), args),
             SerdeGetText::NGetText { ngettext, args } => (ngettext.translate(), args),
+            SerdeGetText::PGetText { pgettext, args } => (pgettext.translate(), args),
+            SerdeGetText::DGetText { dgettext, args } => (dgettext.translate(), args),
+            SerdeGetText::DNGetText { dngettext, args } => (dngettext.translate(), args),
+            SerdeGetText::NPGetText { npgettext, args } => (npgettext.translate(), args),
+            SerdeGetText::DCNGetText { dcngettext, args } => (dcngettext.translate(), args),
         };
 
         Ok(args
@@ -132,6 +150,18 @@ impl Formatter {
 }
 
 #[derive(Deserialize, Clone, Debug)]
+pub struct DatetimeValue {
+    strftime: String,
+    epoch: i64,
+}
+
+impl DatetimeValue {
+    pub fn to_string(&self) -> String {
+        strftime_local(&self.strftime, self.epoch)
+    }
+}
+
+#[derive(Deserialize, Clone, Debug)]
 pub struct SerdeGetTextText(String);
 
 impl SerdeGetTextText {
@@ -163,13 +193,121 @@ impl SerdeGetTextNGetText {
 }
 
 #[derive(Deserialize, Clone, Debug)]
-pub struct DatetimeValue {
-    strftime: String,
-    epoch: i64,
+pub struct SerdeGetTextPGetText {
+    ctx: String,
+    msgid: String,
 }
 
-impl DatetimeValue {
-    pub fn to_string(&self) -> String {
-        strftime_local(&self.strftime, self.epoch)
+impl SerdeGetTextPGetText {
+    pub fn translate(&self) -> String {
+        gettextrs::pgettext(self.ctx.as_bytes(), self.msgid.as_bytes())
+    }
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct SerdeGetTextDGetText {
+    domain: String,
+    msgid: String,
+}
+
+impl SerdeGetTextDGetText {
+    pub fn translate(&self) -> String {
+        gettextrs::dgettext(self.domain.as_bytes(), self.msgid.as_bytes())
+    }
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct SerdeGetTextDNGetText {
+    domain: String,
+    singular: String,
+    plural: String,
+    n: u32,
+}
+
+impl SerdeGetTextDNGetText {
+    pub fn translate(&self) -> String {
+        gettextrs::dngettext(
+            self.domain.as_bytes(),
+            self.singular.as_bytes(),
+            self.plural.as_bytes(),
+            self.n,
+        )
+    }
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct SerdeGetTextNPGetText {
+    ctx: String,
+    singular: String,
+    plural: String,
+    n: u32,
+}
+
+impl SerdeGetTextNPGetText {
+    pub fn translate(&self) -> String {
+        gettextrs::npgettext(
+            self.ctx.as_bytes(),
+            self.singular.as_bytes(),
+            self.plural.as_bytes(),
+            self.n,
+        )
+    }
+}
+
+#[derive(Deserialize, Clone, Debug)]
+pub struct SerdeGetTextDCNGetText {
+    domain: String,
+    singular: String,
+    plural: String,
+    n: u32,
+    category: LocaleCategory,
+}
+
+/// Locale category enum ported from locale.h
+#[derive(Deserialize, Debug, PartialEq, Clone, Copy)]
+pub enum LocaleCategory {
+    /// Character classification and case conversion.
+    LcCType = 0,
+    /// Non-monetary numeric formats.
+    LcNumeric = 1,
+    /// Date and time formats.
+    LcTime = 2,
+    /// Collation order.
+    LcCollate = 3,
+    /// Monetary formats.
+    LcMonetary = 4,
+    /// Formats of informative and diagnostic messages and interactive responses.
+    LcMessages = 5,
+    /// For all.
+    LcAll = 6,
+    /// Paper size.
+    LcPaper = 7,
+    /// Name formats.
+    LcName = 8,
+    /// Address formats and location information.
+    LcAddress = 9,
+    /// Telephone number formats.
+    LcTelephone = 10,
+    /// Measurement units (Metric or Other).
+    LcMeasurement = 11,
+    /// Metadata about the locale information.
+    LcIdentification = 12,
+}
+
+impl std::convert::From<LocaleCategory> for gettextrs::LocaleCategory {
+    fn from(category: LocaleCategory) -> Self {
+        unsafe { std::mem::transmute(category) }
+    }
+}
+
+impl SerdeGetTextDCNGetText {
+    pub fn translate(&self) -> String {
+        gettextrs::dcngettext(
+            self.domain.as_bytes(),
+            self.singular.as_bytes(),
+            self.plural.as_bytes(),
+            self.n,
+            self.category.into(),
+        )
     }
 }
